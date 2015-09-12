21dc0668407af3a95c3e1c87210f84bafb430b0a
Tony Bowden <tony@tmtm.com>
2015-09-12 07:22:22 +0100

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,37 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.css('div.tertiary_content a img').each do |img|
+    div = img.xpath('ancestor::div[1]')
+    name, constituency = div.css('.wp-caption-text text()')
+    data = { 
+      name: name.text.tidy.sub('The Honourable ','').sub(', MLA',''),
+      area: constituency.text.tidy.gsub(/[()]/,'').sub(' Constituency',''),
+      image: img.attr('src'),
+      term: 2013,
+      source: div.css('a/@href').text,
+    }
+    ScraperWiki.save_sqlite([:name, :term], data)
+  end
+end
+
+scrape_list('http://www.falklands.gov.fk/self-governance/legislative/assembly-members/')
