988306d8605d566d7ffcde6882a83a3dea83a6a2
Tony Bowden <tony@tmtm.com>
2017-01-07 11:55:36 +0000

    Clean out database on each run

--- scraper.rb
+++ scraper.rb
@@ -35,4 +35,5 @@ def scrape_list(url)
   end
 end
 
+ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 scrape_list('http://www.falklands.gov.fk/self-governance/legislative/assembly-members/')
