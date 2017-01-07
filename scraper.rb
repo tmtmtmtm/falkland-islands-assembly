41721972cc1b4e51e643b99ee6caaf34e4fa0744
Tony Bowden <tony@tmtm.com>
2017-01-07 11:54:05 +0000

    Remove needless use of colorize

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'nokogiri'
-require 'colorize'
 require 'pry'
+require 'scraperwiki'
+
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
