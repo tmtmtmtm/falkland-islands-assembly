69fc8dc340ea68ffaae68db62381326dd2ee96a5
Tony Bowden <tony@tmtm.com>
2017-01-07 12:34:13 +0000

    Turn on archiving

--- scraper.rb
+++ scraper.rb
@@ -6,8 +6,9 @@ require 'pry'
 require 'scraped'
 require 'scraperwiki'
 
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '.cache'
+require 'scraped_page_archive/open-uri'
 
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
